1c48d2a5fee4dc17861ced57ab51c02c6f65f80c
Keith Maxwell <keith.maxwell@gmail.com>
2024-01-27 22:07:53 +0000

    Use cibuildwheel if the sdist contains '*.c' files
    - Add an example using ctypes
    - Add the cibuildwheel dependency

--- cleanse_metadata_test.py
+++ cleanse_metadata_test.py
@@ -3,30 +3,27 @@
 # SPDX-License-Identifier: MPL-2.0
 import tarfile
 import unittest
-from pathlib import Path
-from shutil import copy
-from tempfile import TemporaryDirectory
 from unittest.mock import patch
 
 from cleanse_metadata import main, parse_args
-from reproducibly_test import ensure_simple_sdist_fixture
+from reproducibly_test import SimpleFixtureMixin
 
 
-class TestMainWithFixture(unittest.TestCase):
+class TestMainWithFixture(SimpleFixtureMixin, unittest.TestCase):
     def test_main_using_fixture(self):
-        with TemporaryDirectory() as tmpdir:
-            sdist = copy(ensure_simple_sdist_fixture(), tmpdir)
-            Path(sdist).rename(f"{sdist}.orig")
-            with tarfile.open(f"{sdist}.orig", "r:gz") as source:
-                with tarfile.open(sdist, "w:gz") as target:
-                    for entry in source.getmembers():
-                        entry.mode = 0o777
-                        target.addfile(entry, source.extractfile(entry))
+        self.sdist.rename(f"{self.sdist}.orig")
+        with (
+            tarfile.open(f"{self.sdist}.orig", "r:gz") as source,
+            tarfile.open(self.sdist, "w:gz") as target,
+        ):
+            for entry in source.getmembers():
+                entry.mode = 0o777
+                target.addfile(entry, source.extractfile(entry))
 
-            returncode = main([sdist])
+        returncode = main([str(self.sdist)])
 
-            with tarfile.open(sdist) as tar:
-                modes = {"0o%o" % tarinfo.mode for tarinfo in tar.getmembers()}
+        with tarfile.open(self.sdist) as tar:
+            modes = {"0o%o" % tarinfo.mode for tarinfo in tar.getmembers()}
         self.assertEqual(returncode, 0)
         self.assertEqual(modes, {"0o755"})
 
