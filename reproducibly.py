f11462436d1d79d01fcce75f383a075947bfc154
Keith Maxwell <keith.maxwell@gmail.com>
2024-06-10 08:52:37 +0100

    Fix type warning about str

--- reproducibly.py
+++ reproducibly.py
@@ -26,7 +26,7 @@ from stat import S_IWGRP, S_IWOTH
 from subprocess import CalledProcessError, run
 from sys import version_info
 from tempfile import TemporaryDirectory
-from typing import cast, TypedDict
+from typing import cast, Literal, TypedDict
 from zipfile import ZipFile, ZipInfo
 
 from build import ProjectBuilder
@@ -84,7 +84,9 @@ CONSTRAINTS = {
 __version__ = "0.0.7"
 
 
-def _build(srcdir: Path, output: Path, distribution: str) -> Path:
+def _build(
+    srcdir: Path, output: Path, distribution: Literal["wheel"] | Literal["sdist"]
+) -> Path:
     """Call the build API
 
     Returns the path to the built distribution"""
